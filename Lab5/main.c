a17bc9901e5e6495b6d4a81d0b4c95a307f7d905
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2019-12-25 21:19:34 +0100

    Update main.c

--- Lab5/main.c
+++ Lab5/main.c
@@ -4,38 +4,46 @@ int main()
 {
     srand(time(0));
     read_csv();
-    FILM ab[1500];
+    FILM ab[1000];
     clock_t start, end;
 
+    ALGORITAM arr[3];
+
+    strcpy(arr[0].name, "SHELL_sort");
     int i = 0;
     for (;i<n;i++){
         ab[i] = niz[i];
     }
-
     start = clock();
     shell_sort(ab);
     end = clock();
     print("Shell_sort.txt", ab);
-    printf("SHELL SORT: %10lf\n", ((double)((double)(end - start)) / (double)CLOCKS_PER_SEC));
+    arr[0].time = (end - start) / ((double)CLOCKS_PER_SEC);
 
+    strcpy(arr[1].name, "QUICK_sort");
     for (i=0;i<n;i++){
         ab[i] = niz[i];
     }
-
     start = clock();
     quick_sort(ab, 0, n-1);
     end = clock();
     print("Quick_sort.txt", ab);
-    printf("QUICK SORT: %10lf\n", (end - start) / (double)CLOCKS_PER_SEC);
+    arr[1].time = (end - start) / ((double)CLOCKS_PER_SEC);
 
+    strcpy(arr[2].name, "HEAP_sort");
     for (i=0;i<n;i++){
         ab[i] = niz[i];
     }
-
     start = clock();
     heap_sort(ab);
     end = clock();
     print("Heap_sort.txt", ab);
-    printf("HEAP SORT: %10lf\n", (end - start) / (double)CLOCKS_PER_SEC);
+    arr[2].time = (end - start) / ((double)CLOCKS_PER_SEC);
+
+    sort_sort(arr);
+
+    for (i=0;i<3;i++){
+        printf("%s %lf\n", arr[i].name, arr[i].time);
+    }
     return 0;
 }
